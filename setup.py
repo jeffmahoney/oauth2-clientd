11d456e74e37af8bc6df17bc4a27977f103583cb
Jeff Mahoney <jeffm@suse.com>
2025-06-17 18:04:41 -0400

    all: convert to pyproject.toml for builds

--- setup.py
+++ setup.py
@@ -4,27 +4,6 @@
 Setup file for installation
 """
 
-import sys
-
 from setuptools import setup
 
-requires = []
-try:
-    import importlib.resources
-except ImportError:
-    requires.append('importlib_resources')
-
-setup(
-    name="oauth2-clientd",
-    version="0.9",
-    python_requires='>=3.6',
-
-    author="Jeff Mahoney",
-    author_email="jeffm@suse.com",
-    description = "OAUTH2 client that caches refresh tokens securely",
-    install_requires=['requests-oauthlib', 'python-daemon', 'cryptography',
-                      'atomicwrites'] + requires,
-    packages = [ "oauth2_clientd", "oauth2_clientd.data" ],
-    package_data={'oauth2_clientd': ['data/*.conf']},
-
-    scripts=["scripts/oauth2-clientd"])
+setup()
